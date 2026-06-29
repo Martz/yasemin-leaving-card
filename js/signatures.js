a9b6abe83643cf7518f1df94d6cd908d2c694a6e
Amina_A <Amina_A@matalan.co.uk>
2026-06-29 10:21:27 +0100

    feat(Card): update signature from Amina

--- js/signatures.js
+++ js/signatures.js
@@ -43,7 +43,7 @@ window.SIGNATURES = [
   },
   {
     name: "Amina",
-    message: "Yasemine, you will be missed dearly, thank you for all your support, I have enjoyed working with you and wish you all the best in your new role!",
+    message: "Yasemin, you will be missed dearly, thank you for all your support, I have enjoyed working with you and wish you all the best in your new role!",
     font: "Permanent Marker",
     colour: "#e290d6",
     rotate: -2,
